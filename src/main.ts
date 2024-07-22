4e03f1ce951a5011f1585eabd8ca47aefb97f88c
desarrollo4@historyclinical.com <desarrollo4@historyclinical.com>
2024-07-22 09:51:05 -0500

    cors setup

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // // Habilitar CORS
-  // app.enableCors({
-  //   origin: 'http://localhost:5173',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  // });
+  // Habilitar CORS
+  app.enableCors({
+    origin: '*', // Permitir cualquier origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
